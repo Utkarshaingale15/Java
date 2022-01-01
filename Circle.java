d62d03223845cf4052fd61cb3edea4035ce717f9
Utkarshaingale15 <92079968+Utkarshaingale15@users.noreply.github.com>
2022-01-01 13:46:24 +0530

    add

---
+++ Circle.java
@@ -0,0 +1,40 @@
+//calculating perimeter and area of circle using interface
+
+import java.util.Scanner;
+import java.lang.Math;
+interface Perimeter
+{
+    double r;
+    void show();
+}
+interface Area
+{
+    void get();
+}
+class Circle implements Perimeter,Area
+{
+    public void show()
+    {
+        double per;
+        per=2*r;
+        System.out.println("Perimeter of circle = "+per);
+    }
+    public void get()
+    {
+        double area;
+        area=Math.PI*r*r;
+        System.out.println("Area of circle = "+area);
+    }
+
+    public static void main(String[] args)
+    {
+        Circle cr=new Circle();
+        Scanner sc=new Scanner(System.in);
+       
+        System.out.println("Enter the radius of circle :-");
+        cr.r=sc.nextDouble();
+
+        cr.show();
+        cr.get();
+    }
+}
