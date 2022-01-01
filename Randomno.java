4e9fb326b053175b2f4b60ee6de426ff5ee733af
Utkarshaingale15 <92079968+Utkarshaingale15@users.noreply.github.com>
2022-01-01 14:54:17 +0530

    add

--- Randomno.java
+++ Randomno.java
@@ -4,3 +4,12 @@ class Randomno{
 		System.out.println("Random number : "+Math.random());
 		}
 	}
+
+
+OUTPUT :-
+	
+	PS F:\Java> javac Randomno.java
+PS F:\Java> java Randomno
+Random number : 0.3439739679195404
+	
+	
