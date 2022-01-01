edec54c2c794e60dd0687f0e773989b9b43ed1bd
Utkarshaingale15 <92079968+Utkarshaingale15@users.noreply.github.com>
2022-01-01 14:31:58 +0530

    commit

--- Linearsearch.java
+++ Linearsearch.java
@@ -34,3 +34,34 @@ class Linearsearch
 
   }  
 }  
+
+
+OUTPUT :-
+   
+   
+   PS F:\Java> javac Linearsearch.java
+PS F:\Java> java Linearsearch
+Enter the number of elements :-
+5
+Enter those 5 elements :-
+98
+12
+67
+9
+34
+Enter the element to find :-
+67
+67 is present in the array at location 3
+PS F:\Java> java Linearsearch
+Enter the number of elements :-
+4
+Enter those 4 elements :-
+78
+34
+9
+12
+Enter the element to find :-
+45
+45 is not present in the array
+
+
